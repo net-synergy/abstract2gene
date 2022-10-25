b784a3968c7c164cebc8552ef92fad970f4347a8
David R Connell <davidconnell12@gmail.com>
2022-10-25 01:31:59 -0500

    Clean: make sure all words start with a letter

--- abstract2gene/nlp.py
+++ abstract2gene/nlp.py
@@ -4,4 +4,4 @@ import nltk
 def tokenize(text):
     "Split raw text into a list of words."
 
-    return nltk.regexp_tokenize(text, r"\w+(?:[-']\w+)*")
+    return nltk.regexp_tokenize(text, r"[a-zA-Z]\w+(?:[-']\w+)*")
