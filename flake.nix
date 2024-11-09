867591bbdedd20fc0a42a6a30552942022ffc2d3
David R Connell <david32@dcon.addy.io>
2024-11-08 23:18:40 -0600

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -2,59 +2,55 @@
   description = "Word distributions related to gene symbols";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    nixpkgs.url = "github:nixos/nixpkgs";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     vim = {
       url = "gitlab:DavidRConnell/vim-container";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, vim }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        a2gEnv = pkgs.poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-          editablePackageSources = { abstract2gene = ./abstract2gene; };
-          preferWheels = true;
-          extraPackages = (ps:
-            with ps; [
-              ipython
-              python-lsp-server
-              pyls-isort
-              python-lsp-black
-              pylsp-mypy
-            ]);
-          groups = [ ];
-        };
-        abstract2gene = pkgs.poetry2nix.mkPoetryPackage { projectDir = ./.; };
-        nixDockerImage = pkgs.dockerTools.buildImage {
-          name = "abstract2gene";
-          tag = "nix";
-          fromImage = vim.packages.${system}.dockerImage;
-          config = {
-            Env = [
-              "HOME=/home/docker"
-              "XDG_DATA_HOME=/home/docker/.local/share"
-              "XDG_CACHE_HOME=/home/docker/.cache"
-              "XDG_CONFIG_HOME=/home/docker/.config"
-            ];
-          };
-          contents = [
-            a2gEnv
-            (pkgs.buildEnv {
-              name = "image-root";
-              pathsToLink = [ "/bin" ];
-              paths = [ pkgs.bashInteractive pkgs.coreutils pkgs.tmux ];
-            })
+  outputs = { self, nixpkgs, poetry2nix, vim }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ poetry2nix.overlays.default ];
+      };
+
+      a2gEnv = pkgs.poetry2nix.mkPoetryEnv {
+        projectDir = ./.;
+        editablePackageSources = { abstract2gene = ./.; };
+        preferWheels = true;
+      };
+
+      nixDockerImage = pkgs.dockerTools.buildImage {
+        name = "abstract2gene";
+        tag = "nix";
+        fromImage = vim.packages.${system}.dockerImage;
+        config = {
+          Env = [
+            "HOME=/home/docker"
+            "XDG_DATA_HOME=/home/docker/.local/share"
+            "XDG_CACHE_HOME=/home/docker/.cache"
+            "XDG_CONFIG_HOME=/home/docker/.config"
           ];
         };
-      in {
-        packages.abstract2gene = abstract2gene;
-        packages.default = self.packages.${system}.abstract2gene;
-        packages.dockerImage = nixDockerImage;
-        devShells.default = pkgs.mkShell { packages = [ a2gEnv pkgs.poetry ]; };
-      });
+        contents = [
+          a2gEnv
+          (pkgs.buildEnv {
+            name = "image-root";
+            pathsToLink = [ "/bin" ];
+            paths = [ pkgs.bashInteractive pkgs.coreutils pkgs.tmux ];
+          })
+        ];
+      };
+    in {
+      packages.dockerImage = nixDockerImage;
+      devShells.${system}.default =
+        pkgs.mkShell { packages = [ a2gEnv pkgs.poetry ]; };
+    };
 }
