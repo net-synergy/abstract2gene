9eb43bce01c74631f6fb648097d0b995bc0d492f
David R Connell <david32@dcon.addy.io>
2024-12-18 23:17:22 -0600

    Update synstore

--- abstract2gene/storage.py
+++ abstract2gene/storage.py
@@ -8,12 +8,7 @@ __all__ = [
     "_storage_factory",
 ]
 
-import os
-from collections.abc import Callable
-from typing import TypeVar
-
 import synstore
-from typing_extensions import ParamSpec
 
 from abstract2gene import __name__ as pkg_name
 
@@ -25,23 +20,4 @@ list_cache = synstore.list_cache
 list_data = synstore.list_data
 delete_from_cache = synstore.delete_from_cache
 delete_from_data = synstore.delete_from_data
-
-P = ParamSpec("P")
-T = TypeVar("T")
-
-
-def _storage_factory(func: Callable[P, T], subdir: str) -> Callable[P, T]:
-    def wrapper(*args: P.args, **kwds: P.kwargs) -> T:
-        if len(args) != 0:
-            new_args = (os.path.join(subdir, args[0]),) + args[1:]  # type: ignore[call-overload]
-        else:
-            new_args = args
-
-        if "path" in kwds and kwds["path"] is not None:
-            kwds["path"] = os.path.join(subdir, kwds["path"])  # type: ignore[call-overload]
-        else:
-            kwds["path"] = subdir
-
-        return func(*new_args, **kwds)
-
-    return wrapper
+_storage_factory = synstore.storage_factory
