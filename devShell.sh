232e7e9cf6e133c46c9b2efc56a411d3bcdcea2f
David R Connell <david32@dcon.addy.io>
2025-01-11 21:26:28 -0600

    CLeanup
    Remove no longer used scripts and modules

--- devShell.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd $(dirname $0)
-
-docker run --rm -v $(pwd):/home/docker/package -v $XDG_DATA_HOME:/home/docker/.local/share -v $XDG_CACHE_HOME:/home/docker/.cache -w /home/docker/package -it abstract2gene:nix ipython
