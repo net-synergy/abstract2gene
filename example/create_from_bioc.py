7d83df87e5d860f07273a1ff491fd74ea7941e0d
David R Connell <david32@dcon.addy.io>
2025-02-12 00:49:05 -0600

    Add all steps needed to create PubTator3 dataset and push to hub

--- example/create_from_bioc.py
+++ example/create_from_bioc.py
@@ -1,7 +1,17 @@
+"""Upload the PubTator3 abstracts dataset to HuggingFace.
+
+Parses the entire PubTator3 BioCXML archive set and converts them to a
+HuggingFace DatasetDict. Each of the original BioCXML files gets its own split
+in the dataset.
+
+Results are pushed to the HuggingFace Hub.
+"""
+
 import os
 import shutil
 
 import datasets
+from huggingface_hub import upload_large_folder
 from huggingface_hub.repocard import DatasetCard, DatasetCardData
 
 from abstract2gene.data import dataset_path
@@ -22,21 +32,34 @@ def clear_save(path):
     os.rmdir(path)
 
 
-save_path = dataset_path("bioc_checkpoint")
-clear_save(save_path)
-dataset = bioc2dataset(range(10), max_cpu=60)
-dataset.save_to_disk(save_path, max_shard_size="250MB")
-
-# dataset = datasets.load_from_disk(save_path)
-dataset = mutators.attach_references(dataset)
-
-clear_save(save_path)
 save_path = dataset_path("bioc")
-dataset.save_to_disk(save_path, max_shard_size="250MB")
+# clear_save(save_path)
 
-dataset.push_to_hub(
+# dataset = bioc2dataset(range(10), max_cpu=60)
+# dataset = mutators.attach_references(dataset)
+
+# dataset.save_to_disk(save_path, max_shard_size="250MB")
+dataset = datasets.load_from_disk(save_path)
+
+os.mkdir(os.path.join(save_path, "data"))
+for k in dataset:
+    source = os.path.join(save_path, k)
+    dest = os.path.join(save_path, "data", k)
+    shutil.move(source, dest)
+
+upload_large_folder(
     "dconnell/pubtator3_abstracts",
+    folder_path=save_path,
+    repo_type="dataset",
+    num_workers=20,
 )
+
+for k in dataset:
+    source = os.path.join(save_path, "data", k)
+    dest = os.path.join(save_path, k)
+    shutil.move(source, dest)
+os.rmdir(os.path.join(save_path, "data"))
+
 
 card_data = DatasetCardData(language="en")
 card = DatasetCard.from_template(
